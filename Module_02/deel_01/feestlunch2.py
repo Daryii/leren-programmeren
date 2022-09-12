2dd0966459a21fd9a8d33f69253a1e66274fbf4c
Daryii <johndoe@example.com>
2022-09-12 13:23:35 +0200

    commit3

---
+++ Module_02/deel_01/feestlunch2.py
@@ -0,0 +1,11 @@
+# Je wilt 17 croissantjes van elk 39 eurocent en 2 stokbroden van elk 2,78 euro kopen. Je hebt 3 kortingsbonnen van 50 eurocent. Hoeveel geld moet je betalen?
+# 17*0.39+2.78*2 = 12.19 - 1.50 = €10.69
+
+aantal = 17
+croissantjes = 0.39
+stokbroden = 2.78
+korting = 1.50
+
+print (aantal*croissantjes + stokbroden*2-korting)
+
+print (f"De feestlunch kost je {aantal*croissantjes + stokbroden*2-korting} euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn")
---
+++ Module_02/deel_01/speelhal2.py
@@ -0,0 +1,11 @@
+# 3 vrienden
+#Je hebt negen 5 minuten in 45 min .
+
+aantal = 3
+ppp = 7.45
+Eurocent = 0.37
+vip_vr = 9 #  er zijn negen 5 minutenn in 45 min....
+
+print((aantal*ppp) + Eurocent*vip_vr*aantal)
+
+print (f"Dit geweldige dagje-uit met 3 mensen in de Speelhal met 45 minuten VR kost je maar {(aantal*ppp) + Eurocent*vip_vr*aantal} euro")
