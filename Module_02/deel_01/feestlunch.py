08a181e9e15c85cb6753e8edb5158ef291711eba
Daryii <johndoe@example.com>
2022-09-12 11:29:31 +0200

    commit!

---
+++ Module_02/deel_01/feestlunch.py
@@ -0,0 +1,10 @@
+# Je wilt 17 croissantjes van elk 39 eurocent en 2 stokbroden van elk 2,78 euro kopen. Je hebt 3 kortingsbonnen van 50 eurocent. Hoeveel geld moet je betalen?
+# 17*0.39+2.78*2 = 12.19 - 1.50 = €10.69
+
+aantal = 17
+croissantjes = 0.39
+stokbroden = 5.56
+korting = 1.50
+
+print (aantal*croissantjes + stokbroden-korting)
+
