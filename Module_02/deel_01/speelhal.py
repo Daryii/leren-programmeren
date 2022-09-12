82ca7f25871932bc6d4fafe5fe62d5e66b494616
Daryii <johndoe@example.com>
2022-09-12 12:12:12 +0200

    commit2

--- Module_02/deel_01/speelhal.py
+++ Module_02/deel_01/speelhal.py
@@ -4,6 +4,6 @@
 aantal = 3
 ppp = 7.45
 Eurocent = 0.37
-vip_vr = 9 #  er zijn negen 5 minutenn in 45 min.
+vip_vr = 9 #  er zijn negen 5 minutenn in 45 min....
 
 print((aantal*ppp) + Eurocent*vip_vr*aantal)
