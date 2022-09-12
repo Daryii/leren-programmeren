c9ed08699801ff87af793dd85b74309d7c4cad07
Daryii <johndoe@example.com>
2022-09-12 12:10:13 +0200

    commitc

---
+++ Module_02/deel_01/speelhal.py
@@ -0,0 +1,9 @@
+# 3 vrienden
+#Je hebt negen 5 minuten in 45 min .
+
+aantal = 3
+ppp = 7.45
+Eurocent = 0.37
+vip_vr = 9 #  er zijn negen 5 minutenn in 45 min.
+
+print((aantal*ppp) + Eurocent*vip_vr*aantal)
