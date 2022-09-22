3333802728a822545059549e9cf5c847b9260c90
Daryii <johndoe@example.com>
2022-09-22 10:27:46 +0200

    commitcm

---
+++ V/challenges/smartphone.py
@@ -0,0 +1,11 @@
+
+prijs_Iphone_13=  int(input("Hoe duur is de Iphone ?"))
+Prijs_Samssung_22= int(input( "Hoe duur is de Samsung ?"))
+
+prijs_i = 1050 
+prijs_s = 1000 
+
+verschil = (prijs_i - prijs_s)
+
+Advies_1 = "De Iphone is het duurst, de telefoon kost:"" 1050 euro en De Samsung is het goedkoopst,de telefoon kost 1000 euro "
+Advies_2 = (str ("Het advies is dus de Iphone te kopen Deze is namelijke "))(prijs_i - prijs_s) #euro goedkoper/duurder dan de Samsung .
