45dcb79b11fbdf43a13e61938bfe7f6cb75eb469
Daryii <johndoe@example.com>
2022-09-26 21:51:28 +0200

    mami

---
+++ Module_02/deel_03/opdracht-2/KassFlow1.py
@@ -0,0 +1,41 @@
+geel= input("Is de kaas geel?")
+if geel == "ja":
+    gaten = input("Zitten er gaten in?")
+    if gaten == "ja":
+        duur = input("Is de kaas belagelijk duur?")
+        if duur == "ja":
+                print ("Emmenthaler")
+                
+        elif duur == "nee":
+                print ("Leerdammer")
+                
+    elif gaten == "nee": 
+        hard = input("Is de kaas hard als steen?")
+        if hard == "ja":
+            print ("Parmigiano Reggiano")
+        elif hard == "nee":
+             print ("Goudse Kaas")
+
+if geel == "nee":
+    blauwe_schimmel = input("Heeft de kaas blauwe schimmel?")
+    if blauwe_schimmel == "ja":
+        korst1= input ("Heeft de kaas een korst1?")
+        if korst1 == "ja":
+            print ("Blue de Rochbaren")
+
+        elif korst1 == "nee":
+            print("foume d'Ambert")
+            
+    elif blauwe_schimmel == "nee":
+        korst = input("Heeft de kaas een korst?")
+        if korst == "ja":
+             print ("Camembert") 
+        elif korst == "nee":
+            print ("Mozzarella") 
+       
+
+                
+
+
+    
+
---
+++ V/klaks.py
@@ -0,0 +1,8 @@
+fyp = input (" wat voor spel is het?")
+if fyp == "ja":
+    bordspel = input ("Is het een bordspel?")
+    if bordspel == "nee":
+        oranje = input("ïs het spel oranje?")
+    if oranje == "ja":
+        print ("het is spel is oranje")
+        elif
