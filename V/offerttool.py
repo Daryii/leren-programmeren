04ca01db816a6917e8feaa683f376028b80efb37
Daryii <johndoe@example.com>
2022-10-13 10:14:10 +0200

    w

--- V/offerttool.py
+++ V/offerttool.py
@@ -2,11 +2,13 @@
 inhoudzwembad = {8*3*1.5} 
 afvoeren_g = {8*3*1.5*32.50}
 uitgraven = {8*3*1.5*25}
+voorrijkosten = {60*1.15+100}
 totaal = (8*3*1.5*32.50 + 8*3*1.5*25)
 
 
 print (f"De hoeveelheid dat afgevoerd moet worden is ",inhoudzwembad,"m3. ")
 offerte = input(f"Wat is de offerte voor een zwembad van 8 bij 3 bij 1,5 geef ant in m3 :")
-print ("Uitgraven:",uitgraven,"")
-print ("Afvoeren grond:",afvoeren_g,"")
-print ("Totaal:",totaal,"")
+print ("Uitgraven: €",uitgraven,"")
+print ("Afvoeren grond: €",afvoeren_g,"")
+print ("Voorrijkosten: €",voorrijkosten,"")
+print ("Totaal: €",totaal,"")
