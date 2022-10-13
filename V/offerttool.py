c41cac71da67583c272f86ce64aa4548fb892b05
Daryii <johndoe@example.com>
2022-10-13 21:10:30 +0200

    w

--- V/offerttool.py
+++ V/offerttool.py
@@ -1,14 +1,29 @@
 
-inhoudzwembad = {8*3*1.5} 
-afvoeren_g = {8*3*1.5*32.50}
-uitgraven = {8*3*1.5*25}
-voorrijkosten = {60*1.15+100}
-totaal = (8*3*1.5*32.50 + 8*3*1.5*25)
+hoogte = int(input("Voe r hier de hoogte in:"))
+breedte = int(input("Voer hier de breedte in:"))
+diepte = float(input("Voer hier de diepte in:"))
+km = int(input("Voer hier de km's in:"))
+
+inhoudzwembad = {hoogte*breedte*diepte} 
+afvoeren_g = {hoogte*breedte*diepte*32.50}
+uitgraven = {hoogte*breedte*diepte*25}
+voorrijkosten = {km*1.15+100}
+totaal = (hoogte*breedte*diepte*32.50 + hoogte*breedte*diepte*25 )
 
 
-print (f"De hoeveelheid dat afgevoerd moet worden is ",inhoudzwembad,"m3. ")
-offerte = input(f"Wat is de offerte voor een zwembad van 8 bij 3 bij 1,5 geef ant in m3 :")
+if km < 50 and inhoudzwembad < 20 :
+    print (100 +1.25)
+    if km >= 50 and inhoudzwembad < 20 :
+       print (100 +1.15)
+elif km < 50 and inhoudzwembad > 20 :
+    print (250+2.15)
+else:
+    print( 250+2.03)
+
+print (f"Offerte als volgt uit :",inhoudzwembad,"m3")
 print ("Uitgraven: €",uitgraven,"")
 print ("Afvoeren grond: €",afvoeren_g,"")
 print ("Voorrijkosten: €",voorrijkosten,"")
 print ("Totaal: €",totaal,"")
+
+
