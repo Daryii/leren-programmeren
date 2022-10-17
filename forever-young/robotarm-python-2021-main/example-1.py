7940845ede71fc86a55b527a7ce007c8cbe49814
Daryii <johndoe@example.com>
2022-10-17 15:25:42 +0200

    w

--- forever-young/robotarm-python-2021-main/example-1.py
+++ forever-young/robotarm-python-2021-main/example-1.py
@@ -2,7 +2,9 @@ from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 1')
 
-robotArm.moveRight();
+robotArm.speed = 3
+
+robotArm.moveRight()
 robotArm.grab()
 robotArm.moveLeft()
 robotArm.drop()
--- forever-young/robotarm-python-2021-main/exmaple-2.py
+++ forever-young/robotarm-python-2021-main/exmaple-2.py
@@ -2,6 +2,8 @@ from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 2')
 
+robotArm.speed = 3
+
 robotArm.grab()
 for i in range (9):
     robotArm.moveRight()
