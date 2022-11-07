dedb0bfc2a6cea9a207bdc33618ea536113dcc93
Daryii <johndoe@example.com>
2022-11-07 15:05:24 +0100

    ë

--- V/pppp.py
+++ V/pppp.py
@@ -1,12 +1,11 @@
+w= ("a = 1 b = 2.2 c = 2 d = 3")
+d = 3
+print(w)
+
 vraag = input("Wat is 2+2-1?")
 
-a = 1
-b=2.2
-c=2
-d=3
 
 if vraag == (d):
-
     print("juist")
 else:
     print("onjuist")
--- forever-young/robotarm-python-2021-main/example-7.py
+++ forever-young/robotarm-python-2021-main/example-7.py
@@ -1,16 +1,11 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 7')
-robotArm.speed = 2
-
-for g in range(1,3):
-    robotArm.moveRight()
-for i in range(1,6):
-    for r in range(1,7):
+for i in range(5):
+    for r in range(6):
         robotArm.moveRight()
         robotArm.grab()
         robotArm.moveLeft()
         robotArm.drop()
-
-
- 
-    robotArm.wait()
+    for g in range(2):
+        robotArm.moveRight()
+robotArm.wait()
