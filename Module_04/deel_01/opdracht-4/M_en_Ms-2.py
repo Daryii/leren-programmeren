46e332ff3c7c6756505f7e3e60d60f53c0a10ffc
Daryi <d.kiflai10@gmail.com>
2022-12-05 12:53:25 +0100

    w

---
+++ Module_04/deel_01/opdracht-4/M_en_Ms-2.py
@@ -0,0 +1,16 @@
+import random
+kleuren = ["rood","blauw","groen","geel","bruin"]
+
+vraag = int(input("Hoeveel M&Ms er aan de zak toegevoegd worden?: "))
+zak = {} 
+
+getal = 1
+for u in range(vraag):
+    random_k = random.choice(kleuren)
+    if random_k not in zak:
+        zak.update({random_k: getal})
+    elif random_k in zak:
+        zak[random_k] +=1
+        
+print(zak)
+
