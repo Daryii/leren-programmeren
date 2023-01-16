1e05379169e803a1e27ecb72101b6014cfb74aac
Daryii <112935597+Daryii@users.noreply.github.com>
2023-01-16 14:13:13 +0100

    updated

--- Module_05/Deel_1/fibonacci.py
+++ Module_05/Deel_1/fibonacci.py
@@ -1,9 +1,10 @@
-def fibonacci(aantal):
+def fibonacci(aantal : int) -> float:
     lijst = [0,1]
-    for i in lijst:
-        lijst.append(aantal)
-    return aantal
+    for i in range(2, aantal):
+        lijst.append(lijst[-1] + lijst[-2])
+    gulden_snede = ((lijst[-1]) / lijst[-2])
+    print(lijst)
+    return gulden_snede
     
-fibonacci()
 
-        
+print(fibonacci(230))
