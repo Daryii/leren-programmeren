5a7bfdd4afec15c7600d054a3fdd9bf3deb6a037
Daryii <112935597+Daryii@users.noreply.github.com>
2023-01-30 12:57:19 +0100

    eerste commit van fibonacci 2

--- Module_05/Deel_1/fibonacci.py
+++ Module_05/Deel_1/fibonacci.py
@@ -3,8 +3,7 @@ def fibonacci(aantal : int) -> float:
     for i in range(2, aantal):
         lijst.append(lijst[-1] + lijst[-2])
     gulden_snede = ((lijst[-1]) / lijst[-2])
-    print(lijst)
     return gulden_snede
     
 
-print(fibonacci(285))
+print(fibonacci(5))
--- V/ex.py
+++ V/ex.py
@@ -1,2 +1,9 @@
 keuzen = ('a','b','c','d','e','f','g','h')
 
+def getal_maal_dire(getal: int, max: int) -> int:
+    if getal >= max:
+        return getal
+    else:
+       return getal_maal_dire(3,1000)
+
+print(getal_maal_dire)
