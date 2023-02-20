a47e96df492c3078434e86599cc521e6e589791a
Daryii <112935597+Daryii@users.noreply.github.com>
2023-02-20 11:01:17 +0100

    updated

--- Module_05/Deel_1/Berekeningen_uitvoeren.py
+++ Module_05/Deel_1/Berekeningen_uitvoeren.py
@@ -10,148 +10,73 @@ def multiplication(n1, n2):
 def division(n1, n2):
     return n1 / n2
 
-def vraag_2(uitkomst):
-    uitkomst1 = uitkomst
-    keuzen_uitkomst = input(f"Wil je wat met de uitkomst ({uitkomst1}) doen? a) iets optellen, b) iets aftrekken, c) met iets vermenigvuldigen, d) ergens door delen, e) ophogen, f) verlagen, g) verdubbelen, h) halveren of i) niets? ").lower()
-    return keuzen_uitkomst
+
+keuze = " "
+while not keuze in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'):
+    keuze = input("Wat wilt u doen? A) getallen optellen, B) getallen aftrekken, C) getallen vermenigvuldigen, D) getallen delen, E) getal ophogen, F) getal verlagen, G) getal verdubbelen of H) getal halveren of I) Nee?: ").lower()
+    if keuze == "i":
+        exit()
 
 
-keuzen = input("Wat wilt u doen? a) getallen optellen, b) getallen aftrekken, c) getallen vermenigvuldigen, d) getallen delen, e) getal ophogen, f) getal verlagen, g) getal verdubbelen of h) getal halveren? of i)om helemaal te stoppen: ").lower()
-
-n1 = float(input("Welke getal: "))
-
+aantal = -1
 while True:
-    n2 = float(input(f"Welke getal optellen bij {n1} : "))
-    uitkomst = addition(n1, n2)
-    print(f"{n1} + {n2} = {uitkomst}")
-    n1 = uitkomst
-    keuzen_uitkomst = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? a) iets optellen, b) iets aftrekken, c) met iets vermenigvuldigen, d) ergens door delen, e) ophogen, f) verlagen, g) verdubbelen, h) halveren of i) niets? ").lower()
+    if aantal > -1 and keuze != 'i' and keuze in ('a', 'b', 'c', 'd'):
+        n1 = aantal
+        n2 = int(input("Geef de 2e getal op: "))
 
+    elif aantal > -1 and keuze != 'i' and keuze in ('e', 'f', 'g', 'h'):
+        n1 = aantal
 
-# test = False
+        
+    elif keuze in ('a', 'b', 'c', 'd'):
+        n1 = int(input("Geef een getal op: "))
+        n2 = int(input("Geef nog een getal op: "))
 
-# while True:
-#     if not test:
-#         n1 = float(input("Welke getal: "))
-#         test = True
-#     if keuzen == "a":
+    elif keuze in ('e', 'f', 'g', 'h'):
+        n1 = int(input("Geef een getal op: "))
 
-#         
-#         uitkomst = addition(n1, n2)
-#         print(f"{n1} + {n2} = {uitkomst}")
-#         print("______________________________________")
-#         vraag_2(uitkomst)
-#         n2a = float(input(f"Welke getal optellen bij {uitkomst} : "))
-#         brekening = uitkomst + n2a
-#         print(f"{brekening}")
-#         print("_______________________________________")
-        #vraag_2(brekening)
+    else: 
+        exit()
 
+    if keuze == "a":
+        aantal = addition(n1, n2)
+        print(aantal)
+    elif keuze == "b":
+        aantal = subtraction(n1, n2)
+        print(aantal)
+    elif keuze == "c":
+        aantal = multiplication(n1, n2)
+        print(aantal)
+    elif keuze == "d":
+        aantal = division(n1, n2)
+        print(aantal)
+    elif keuze == "e":
+        n2 = 1
+        aantal = addition(n1, n2)
+        print(aantal)
+    elif keuze == "f":
+        n2 = 1
+        aantal = subtraction(n1, n2)
+        print(aantal)
+    elif keuze == "g":
+        n2 = 2
+        aantal = multiplication(n1, n2)
+        print(aantal)
+    else:
+        n2 = 2
+        aantal = division(n1, n2)
+        print(aantal)
 
-#     elif keuzen == "b":
-#         n1 = float(input("Welke getal: "))
-#         n2 = float(input(f"Welke getal aftrekken bij {n1} : "))
-#         uitkomst = subtraction(n1, n2)
-#         print(f"{n1} - {n2} = {uitkomst}")
-#         print("______________________________________")
-
-#     elif keuzen == "c":
-#         n1 = float(input("Welke getal: "))
-#         n2 = float(input(f"Welke getal vermenigvuldigen bij {n1} : "))
-#         uitkomst = multiplication(n1, n2)
-#         print(f"{n1} * {n2} = {uitkomst}")
-#         print("______________________________________")
-
-
-#     elif keuzen == "d":
-#         n1 = float(input("Welke getal: "))
-#         n2 = float(input(f"Welke getal delen bij {n1} : "))
-#         uitkomst = division(n1, n2)
-#         print(f"{n1} / {n2} = {uitkomst}")
-#         print("______________________________________")
-
-#     elif keuzen == "e":
-#         n1 = float(input("Getal: "))
-#         n2 = 1
-#         uitkomst = addition(n1, n2)
-#         print(f"{n1} + {n2} = {uitkomst}")
-#         print("______________________________________")
-
-#     elif keuzen == "f":
-#         n1 = float(input("Getal: "))
-#         n2 = 1
-#         uitkomst = subtraction(n1, n2)
-#         print(f"{n1} - {n2} = {uitkomst}")
-#         print("______________________________________")
-
-#     elif keuzen == "g":
-#         n1 = float(input("Getal: "))
-#         n2 = 2
-#         uitkomst = multiplication(n1, n2)
-#         print(f"{n1} * {n2} = {uitkomst}")
-#         print("______________________________________")
-
-#     elif keuzen == "h":
-#         n1 = float(input("Getal: "))
-#         n2 = 2
-#         uitkomst = division(n1, n2)
-#         print(f"{n1} / {n2} = {uitkomst}")
-#         print("______________________________________")
+    keuze = input(f"Wil je wat met het antwoord ({aantal}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) Nee?")
+    
 
 
 
-#     # if keuzen_uitkomst == "a":
+
+    
 
 
-#     # elif keuzen_uitkomst == "b":
-#     #     n2a = float(input(f"Welke getal aftrekken van {uitkomst1}?:  "))
-#     #     if n2a > uitkomst1:
-#     #         print("Dat is geen geldig getal! ")
-#     #     else:
-#     #         brekening = uitkomst1 - n2a
-#     #         print(f"{brekening}")
-#     #         print("______________________________________")
 
 
-#     # elif keuzen_uitkomst == "c":
-#     #     n2a = float(input(f"Welke getal vermenigvuldigen met {uitkomst1}?:  "))
-#     #     brekening = uitkomst1 * n2a
-#     #     print(f"{brekening}")
-#     #     print("______________________________________")
-
-#     # elif keuzen_uitkomst == "d":
-#     #     n2b = float(input(f"Welke getal delen met {uitkomst1}?:  "))
-#     #     if n2b == 0:
-#     #         print("kan niet door 0 delen")
-#     #     else:
-#     #         brekening = uitkomst1 / n2a
-#     #         print(f"{brekening}")
-#     #         print("______________________________________")
-
-#     # elif keuzen_uitkomst == "e":
-#     #     print("getal ophogen: ")
-#     #     uitkomst1 = uitkomst + 1
-#     #     print(f"{uitkomst1}")
-#     #     print("______________________________________")
-
-#     # elif keuzen_uitkomst == "f":
-#     #     print("getal verlagen: ")
-#     #     uitkomst1 = uitkomst - 1
-#     #     print(f"{uitkomst1}")
-#     #     print("______________________________________")
-
-#     # elif keuzen_uitkomst == "g":
-#     #     print("getal verdubbelen: ")
-#     #     uitkomst1 = uitkomst * 2
-#     #     print(f"{uitkomst1}")
-#     #     print("______________________________________")
 
 
-#     # elif keuzen_uitkomst == "h":
-#     #     print("getal halveren: ")
-#     #     uitkomst1 = uitkomst / 2
-#     #     print(f"{uitkomst1}")
-#     #     print("______________________________________")
-
-#     # elif keuzen_uitkomst or keuzen  == "i":
-#     #     break
--- V/ex.py
+++ V/ex.py
@@ -1,22 +1,37 @@
 def addition(n1, n2):
     return n1 + n2
 
+def subtraction(n1, n2):
+    return n1 - n2
 
-def vraag_2(uitkomst):
-    keuzen_uitkomst = input(f"Wil je wat met de uitkomst ({uitkomst}) doen? a) iets optellen, b) iets aftrekken, c) met iets vermenigvuldigen, d) ergens door delen, e) ophogen, f) verlagen, g) verdubbelen, h) halveren of i) niets? ").lower()
-    return keuzen_uitkomst
+def multiplication(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    return n1 / n2
 
 
-keuzen = input("Wat wilt u doen? a) getallen optellen, b) getallen aftrekken, c) getallen vermenigvuldigen, d) getallen delen, e) getal ophogen, f) getal verlagen, g) getal verdubbelen of h) getal halveren? of i) om helemaal te stoppen: ").lower()
+keuzen = input("Wat wilt u doen? a) getallen optellen, b) getallen aftrekken, c) getallen vermenigvuldigen, d) getallen delen, e) getal ophogen, f) getal verlagen, g) getal verdubbelen of h) getal halveren? of i)om helemaal te stoppen: ").lower()
 
-test = False
-uitkomst = 0
+n1 = float(input("Welke getal: "))
 
 while True:
-    if not test:
-        n1 = float(input("Welk getal: "))
-        test = True
     if keuzen == "a":
-        n2 = float(input(f"Welk getal wilt u optellen bij {n1}: "))
+        n2 = float(input(f"Welke getal optellen bij {n1} : "))
         uitkomst = addition(n1, n2)
         print(f"{n1} + {n2} = {uitkomst}")
+        print("______________________________________")
+
+    n1 = uitkomst
+    keuzen_uitkomst = input(f"Wat wil je wat met de uitkomst ({uitkomst}) doen? a) iets optellen, b) iets aftrekken, c) met iets vermenigvuldigen, d) ergens door delen, e) ophogen, f) verlagen, g) verdubbelen, h) halveren of i) niets? ").lower()
+
+    while keuzen_uitkomst != "i":
+        if keuzen_uitkomst == "a":
+            vraag = float(input(f"Welke getal optellen bij {uitkomst}?:  "))
+            brekening = uitkomst + vraag
+            print(f"{uitkomst} + {vraag} = {brekening}")
+            print("______________________________________")
+        elif keuzen_uitkomst == "b":
+            vraag = float(input(f"Welke getal aftrekken van {uitkomst}?:  "))
+            brekening = uitkomst - vraag
+            print(f"{uitkomst} - {vraag} = {break}")
