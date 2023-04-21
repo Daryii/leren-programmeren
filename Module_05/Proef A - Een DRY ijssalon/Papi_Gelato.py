b5990cf6c7676c268b9d29bd84498875b86cee28
Daryii <112935597+Daryii@users.noreply.github.com>
2023-04-21 15:07:15 +0200

    tweede commit

--- Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
+++ Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
@@ -1,42 +1,58 @@
-def Papi_Gelato_1():
-        
-    print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is")
+aantal_totaal = 0
 
-    aantal_bolletjes = int(input("Hoeveel bolletjes wilt u? :"))
-    aantal = ''
+def print_bestelling():
+    print(f"U heeft in totaal {aantal_totaal} bolletjes besteld. Bedankt en tot ziens!")
+
+def bestel_bolletjes():
+    global aantal_totaal
+    aantal_bolletjes = input("Hoeveel bolletjes wilt u? : ")
+    if not aantal_bolletjes.isdigit():
+        print("Sorry, dat is geen geldig aantal bolletjes.")
+        bestel_bolletjes()
+        return
+    
+    aantal_bolletjes = int(aantal_bolletjes)
+
+    
     if aantal_bolletjes <= 3 and aantal_bolletjes > 0:
-        aantal = aantal_bolletjes
+        aantal_totaal += aantal_bolletjes
         c = True
         while c == True:
-            vraag = input(f"Wilt u deze {aantal} bolletjes in een hoorntje of een bakje?: ")
+            vraag = input(f"Wilt u deze {aantal_totaal} bolletjes in een hoorntje of een bakje?: ")
             if vraag == "bakje" or vraag == "hoorntje":
                 c = False
-                print(f"Hier is uw {vraag} met {aantal} bolletjes")
+                print(f"Hier is uw {vraag} met {aantal_totaal} bolletjes")
             else:
                 print("Sorry,dat snap ik niet...")
         
-    elif aantal_bolletjes == 4 and aantal_bolletjes <= 8:
-        print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes')
+    elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
+        aantal_totaal += aantal_bolletjes
+        print(f'Dan krijgt u van mij een bakje met {aantal_totaal} bolletjes')
     elif aantal_bolletjes > 8:
         print("Sorry, zulke grotebakken hebben we niet")
-        Papi_Gelato_1()
+        bestel_bolletjes()
     else:
         exit()
-Papi_Gelato_1()
-
-v = True
-while v == True:
-    bestelling_vraag = input("Wilt u nog meer bestellen?: ")
-    if bestelling_vraag == "ja":
-        Papi_Gelato_1()
-        v = False
-    elif bestelling_vraag == "nee":
-        print("Bedankt en tot ziens!")
-        v = False
-    else:
-        print("Sorry,dat snap ik niet...")
-        
-        
 
 
-        
+
+def bestel_meer():
+    while True:
+        bestelling_vraag = input("Wilt u nog meer bestellen?: ")
+        if bestelling_vraag == "ja":
+            bestel_bolletjes()
+        elif bestelling_vraag == "nee":
+            print_bestelling()
+            break
+        else:
+            print("Sorry,dat snap ik niet...")
+
+def start():
+    print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is")
+    bestel_bolletjes()
+    bestel_meer()
+
+start()
+            
+
+
