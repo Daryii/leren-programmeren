4da6df5ad1183d804a5ec23f6ada0765089a7724
Daryii <112935597+Daryii@users.noreply.github.com>
2023-04-26 16:51:14 +0200

    commit pseudocode

--- Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
+++ Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
@@ -1,33 +1,20 @@
-aantal_totaal = 0
-
-def print_bestelling():
-    print(f"U heeft in totaal {aantal_totaal} bolletjes besteld. Bedankt en tot ziens!")
+totaal = 0
 
 def bestel_bolletjes():
-    global aantal_totaal
-    aantal_bolletjes = input("Hoeveel bolletjes wilt u? : ")
-    if not aantal_bolletjes.isdigit():
-        print("Sorry, dat is geen geldig aantal bolletjes.")
-        bestel_bolletjes()
-        return
-    
-    aantal_bolletjes = int(aantal_bolletjes)
-
-    
+    aantal_bolletjes = int(input("Hoeveel bolletjes wilt u? : "))
     if aantal_bolletjes <= 3 and aantal_bolletjes > 0:
-        aantal_totaal += aantal_bolletjes
+        
         c = True
         while c == True:
-            vraag = input(f"Wilt u deze {aantal_totaal} bolletjes in een hoorntje of een bakje?: ")
+            vraag = input(f"Wilt u deze {aantal_bolletjes} bolletjes in een hoorntje of een bakje?: ")
             if vraag == "bakje" or vraag == "hoorntje":
                 c = False
-                print(f"Hier is uw {vraag} met {aantal_totaal} bolletjes")
+                print(f"Hier is uw {vraag} met {aantal_bolletjes} bolletjes")
             else:
                 print("Sorry,dat snap ik niet...")
         
     elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
-        aantal_totaal += aantal_bolletjes
-        print(f'Dan krijgt u van mij een bakje met {aantal_totaal} bolletjes')
+        print(f'Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes')
     elif aantal_bolletjes > 8:
         print("Sorry, zulke grotebakken hebben we niet")
         bestel_bolletjes()
@@ -37,12 +24,13 @@ def bestel_bolletjes():
 
 
 def bestel_meer():
+    global totaal
     while True:
         bestelling_vraag = input("Wilt u nog meer bestellen?: ")
         if bestelling_vraag == "ja":
             bestel_bolletjes()
         elif bestelling_vraag == "nee":
-            print_bestelling()
+            print(f"U heeft in totaal {totaal} bolletjes besteld. Bedankt en tot ziens!")
             break
         else:
             print("Sorry,dat snap ik niet...")
