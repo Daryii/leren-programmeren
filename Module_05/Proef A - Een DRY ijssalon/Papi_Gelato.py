e42260800f4544df36813db722f312ddae54b9eb
Daryii <112935597+Daryii@users.noreply.github.com>
2023-05-10 12:57:33 +0200

    commit

--- Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
+++ Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
@@ -1,36 +1,37 @@
-totaal = 0
-
 def bestel_bolletjes():
     aantal_bolletjes = int(input("Hoeveel bolletjes wilt u? : "))
-    if aantal_bolletjes <= 3 and aantal_bolletjes > 0:
-        
+   
+    if aantal_bolletjes <= 3 and aantal_bolletjes > 0:  
         c = True
         while c == True:
             vraag = input(f"Wilt u deze {aantal_bolletjes} bolletjes in een hoorntje of een bakje?: ")
             if vraag == "bakje" or vraag == "hoorntje":
                 c = False
-                print(f"Hier is uw {vraag} met {aantal_bolletjes} bolletjes")
             else:
                 print("Sorry,dat snap ik niet...")
         
     elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
         print(f'Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes')
+        bestel_meer()
     elif aantal_bolletjes > 8:
         print("Sorry, zulke grotebakken hebben we niet")
         bestel_bolletjes()
+        return
     else:
-        exit()
+        print("Sorry,dat snap ik niet...")
+        return
+        
 
 
 
-def bestel_meer():
-    global totaal
+def bestel_meer(aantal_bolletjes,vraag):
     while True:
         bestelling_vraag = input("Wilt u nog meer bestellen?: ")
         if bestelling_vraag == "ja":
             bestel_bolletjes()
         elif bestelling_vraag == "nee":
-            print(f"U heeft in totaal {totaal} bolletjes besteld. Bedankt en tot ziens!")
+           
+            print(f"Hier is uw {vraag} met {aantal_bolletjes} bolletjes")
             break
         else:
             print("Sorry,dat snap ik niet...")
--- V/extraa/ex.py
+++ V/extraa/ex.py
@@ -1,3 +1,10 @@
-a = 1.7
-b = round(a)
-print(b)
+dict_a = {'a': 1, 'b': 2}
+
+
+dict_b = dict_a
+
+
+dict_b['a'] = 3
+
+
+print(dict_a)
