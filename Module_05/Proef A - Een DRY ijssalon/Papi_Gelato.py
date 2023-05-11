dec7d09cabe653fcb10ad1784428af9cf59833a1
Daryii <112935597+Daryii@users.noreply.github.com>
2023-05-11 15:00:06 +0200

    commit

--- Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
+++ Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
@@ -1,7 +1,17 @@
-def bestel_bolletjes():
+def get_ijs(bestellingen) -> list:
+    ijs_dict = {}
+    vraag = ""
     aantal_bolletjes = int(input("Hoeveel bolletjes wilt u? : "))
-   
-    if aantal_bolletjes <= 3 and aantal_bolletjes > 0:  
+
+    a = True
+    while a == True:
+        if aantal_bolletjes != int:
+            a = False
+        else:
+            print("voer een getal in!")
+            a = True
+
+    if aantal_bolletjes <= 3 and aantal_bolletjes > 0:
         c = True
         while c == True:
             vraag = input(f"Wilt u deze {aantal_bolletjes} bolletjes in een hoorntje of een bakje?: ")
@@ -9,39 +19,35 @@ def bestel_bolletjes():
                 c = False
             else:
                 print("Sorry,dat snap ik niet...")
-        
+
     elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
         print(f'Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes')
-        bestel_meer()
+
     elif aantal_bolletjes > 8:
-        print("Sorry, zulke grotebakken hebben we niet")
-        bestel_bolletjes()
-        return
+        print("Sorry, zulke grote bakken hebben we niet")
+        return bestellingen
     else:
         print("Sorry,dat snap ik niet...")
-        return
-        
+        return bestellingen
 
+    ijs_dict["bolletjes"] = aantal_bolletjes
+    ijs_dict["keuzen"] = vraag
+    bestellingen.append(ijs_dict)
 
-
-def bestel_meer(aantal_bolletjes,vraag):
     while True:
         bestelling_vraag = input("Wilt u nog meer bestellen?: ")
         if bestelling_vraag == "ja":
-            bestel_bolletjes()
+            get_ijs(bestellingen)
         elif bestelling_vraag == "nee":
-           
-            print(f"Hier is uw {vraag} met {aantal_bolletjes} bolletjes")
-            break
+            print("Hier zijn uw bestellingen:")
+            for i in bestellingen:
+                if i in ijs_dict["keuzen"]:
+                    print(f"{i['bolletjes']} bolletjes in een {i['keuzen']}")
+            else:
+                print(f"{i['bolletjes']} bolletjes in een bakje")
+            return bestellingen
         else:
             print("Sorry,dat snap ik niet...")
-
-def start():
-    print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is")
-    bestel_bolletjes()
-    bestel_meer()
-
-start()
             
-
-
+orders = []
+get_ijs(orders)
