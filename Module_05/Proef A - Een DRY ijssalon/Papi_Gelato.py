e5e5c0ad02a04a87de6bef2caf65ac56607d6365
Daryii <112935597+Daryii@users.noreply.github.com>
2023-05-12 12:53:54 +0200

    commit
    1

--- Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
+++ Module_05/Proef A - Een DRY ijssalon/Papi_Gelato.py
@@ -1,53 +1,73 @@
-def get_ijs(bestellingen) -> list:
-    ijs_dict = {}
-    vraag = ""
-    aantal_bolletjes = int(input("Hoeveel bolletjes wilt u? : "))
 
+#om het aantal bolletjes op te vragen
+def get_aantal_bolletjes(vraag :str)-> int:
+
+    aantal = int(input(vraag))
     a = True
     while a == True:
-        if aantal_bolletjes != int:
+        if aantal != int:
             a = False
         else:
             print("voer een getal in!")
             a = True
+    return aantal
+
+
+def get_hoorntje_of_bakje(aantal_bolletjes):
+    c = True
+    while c == True:
+        vraag = input(f"Wilt u deze {aantal_bolletjes} bolletjes in een hoorntje of een bakje?: ")
+        if vraag == "bakje" or vraag == "hoorntje":
+            c = False
+        else:
+            print("Sorry,dat snap ik niet...")
+        return aantal_bolletjes
+    
+#maakt een ijsje een geeft het terug
+def get_ijs() -> list:
+    
+    ijs_dict = {}
+    vraag = ""
+    
+
+    aantal_bolletjes  = get_aantal_bolletjes("Hoeveel bolletjes wilt u? : ")
 
     if aantal_bolletjes <= 3 and aantal_bolletjes > 0:
-        c = True
-        while c == True:
-            vraag = input(f"Wilt u deze {aantal_bolletjes} bolletjes in een hoorntje of een bakje?: ")
-            if vraag == "bakje" or vraag == "hoorntje":
-                c = False
-            else:
-                print("Sorry,dat snap ik niet...")
-
+        
+            
     elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
         print(f'Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes')
-
+   
     elif aantal_bolletjes > 8:
         print("Sorry, zulke grote bakken hebben we niet")
-        return bestellingen
+        
     else:
         print("Sorry,dat snap ik niet...")
-        return bestellingen
+       
 
     ijs_dict["bolletjes"] = aantal_bolletjes
     ijs_dict["keuzen"] = vraag
-    bestellingen.append(ijs_dict)
+    return ijs_dict     
 
-    while True:
-        bestelling_vraag = input("Wilt u nog meer bestellen?: ")
-        if bestelling_vraag == "ja":
-            get_ijs(bestellingen)
-        elif bestelling_vraag == "nee":
-            print("Hier zijn uw bestellingen:")
-            for i in bestellingen:
-                if i in ijs_dict["keuzen"]:
-                    print(f"{i['bolletjes']} bolletjes in een {i['keuzen']}")
-            else:
-                print(f"{i['bolletjes']} bolletjes in een bakje")
-            return bestellingen
-        else:
-            print("Sorry,dat snap ik niet...")
+    
             
 orders = []
-get_ijs(orders)
+
+print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is")
+orders.append(get_ijs())
+
+
+d = True
+while d == True:
+    bestelling_vraag = input("Wilt u nog meer bestellen?: ")
+    if bestelling_vraag == "ja":
+        orders.append(get_ijs())
+        print(orders)
+    elif bestelling_vraag == "nee":
+        print("Hier zijn uw bestellingen:")
+        for i in orders:
+            print(f"{i['bolletjes']} bolletjes in een {i['keuzen']}")
+        d = False
+    else:
+        print("Sorry,dat snap ik niet...")
+
